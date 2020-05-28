c03182574b8aaf1993a2e41c0836b5f3e3002a1b
Eugene <eugene.j.leroux@gmail.com>
2020-05-28 10:26:05 +0200

    Add the image collage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,188 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>This is the photography website</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+      <h1>Photo Gallary Grid</h1>
+      <div className='photo-grid'>
+        <div
+          className='card card-wide'
+          style={{
+            color: 'white',
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          1
+        </div>
+        <div
+          className='card card-tall'
+          style={{
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1518495973542-4542c06a5843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          2
+        </div>
+        <div
+          className='card'
+          style={{
+            color: 'white',
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1546823089-c22356dc7d86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          3
+        </div>
+        <div
+          className='card card-wide'
+          style={{
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1494959764136-6be9eb3c261e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          4
+        </div>
+        <div
+          className='card'
+          style={{
+            color: 'white',
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          5
+        </div>
+        <div
+          className='card card-tall'
+          style={{
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          6
+        </div>
+        <div
+          className='card'
+          style={{
+            color: 'white',
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1527061011665-3652c757a4d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          7
+        </div>
+        <div
+          className='card'
+          style={{
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1547617774-2eb320ad93f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          8
+        </div>
+        <div
+          className='card'
+          style={{
+            color: 'white',
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          9
+        </div>
+        <div
+          className='card'
+          style={{
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1546931665-73db142e716f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          10
+        </div>
+        <div
+          className='card'
+          style={{
+            color: 'white',
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1481819613568-3701cbc70156?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          11
+        </div>
+        <div
+          className='card card-wide'
+          style={{
+            backgroundImage:
+              'url(' +
+              'https://images.unsplash.com/photo-1530008017790-9b1ab6878a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=635&q=80' +
+              ')',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          12
+        </div>
+      </div>
     </div>
   );
 }
